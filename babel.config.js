d6d82aaac9a9c3579521a5c0ca91fe6094a263b8
aKabir <akabir@innovaj.com>
2025-11-11 15:39:53 +0000

    fix: absolute path resolution error

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+          '@app': './src/app',
+          '@features': './src/features',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
 };
