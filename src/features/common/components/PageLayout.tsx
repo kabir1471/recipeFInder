ddcc3bbeb7b0c0d05d2345e5bb679a717f22dbe0
aKabir <akabir@innovaj.com>
2025-11-11 16:25:53 +0000

    feat: create a page layout wrapper

---
+++ src/features/common/components/PageLayout.tsx
@@ -0,0 +1,20 @@
+import React, { FC, PropsWithChildren } from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const PageLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+	<View style={styles.container}>
+	  {children}
+	</View>
+  )
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingVertical: 16,
+		backgroundColor: '#fff',
+	}
+})
+
+export default PageLayout
