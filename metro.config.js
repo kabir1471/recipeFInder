4d8065ab0788470cb5eaef823ee2790d14880ee8
aKabir <akabir@innovaj.com>
2025-11-11 15:07:49 +0000

    chore: add config for absolute paths

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -6,6 +7,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+	resolver: {
+		alias: {
+			'@app': path.resolve(__dirname, 'src/app'),
+			'@features': path.resolve(__dirname, 'src/features'),
+			'@types': path.resolve(__dirname, 'src/types'),
+		},
+  	},
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
